a56634cd35f3f736ad9a0634389edca761ab36ab
lijie <confucians2012@163.com>
2018-01-17 15:23:02 +0800

    config server1

---
+++ src/main/java/com/wk/ConfigServerApplication.java
@@ -0,0 +1,26 @@
+package com.wk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * ClassName ConfigServerApplication
+ * Describe  配置中心服务
+ *
+ * @author lijie
+ * @create 2018-01-17 10:35
+ * 获取git上的资源信息遵循如下规则：
+ *  /{application}/{profile}[/{label}]
+    /{application}-{profile}.yml
+    /{label}/{application}-{profile}.yml
+    /{application}-{profile}.properties
+    /{label}/{application}-{profile}.properties
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class);
+    }
+}
