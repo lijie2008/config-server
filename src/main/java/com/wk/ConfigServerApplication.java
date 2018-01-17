1a2decbcc8c86ee9c66b38ed04d1fa94ae10ae3c
lijie <confucians2012@163.com>
2018-01-17 15:57:17 +0800

    修改

--- src/main/java/com/wk/ConfigServerApplication.java
+++ src/main/java/com/wk/ConfigServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+
 /**
  * ClassName ConfigServerApplication
  * Describe  配置中心服务
